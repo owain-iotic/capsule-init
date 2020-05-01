c476162f8388c203ddc5f28e736fcd691e5eeed4
owain.perry <owain.perry@iotic-labs.com>
2020-05-01 14:21:20 +0100

    remove public ip

--- setup.sh
+++ setup.sh
@@ -5,8 +5,9 @@ echo "kubeadm init\n\n"
 
 TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"` 
 PRIVATE_IP=`curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/local-ipv4`
-PUBLIC_IP=`curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/public-ipv4`
-SANS="$PRIVATE_IP,$PUBLIC_IP"
+#PUBLIC_IP=`curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/public-ipv4`
+#SANS="$PRIVATE_IP,$PUBLIC_IP"
+SANS="$PRIVATE_IP"
 echo $SANS
 kubeadm init --apiserver-cert-extra-sans="$SANS"
 
